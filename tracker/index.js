4f6f4619bb92e17daf7e05f72ed023e40e67785e
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-17 22:55:42 +0200

    i thunk + redux logger

--- tracker/index.js
+++ tracker/index.js
@@ -1,4 +1,16 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import reducer from './store/reducer';
 
-AppRegistry.registerComponent('tracker', () => App);
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+const AppContainer = () =>
+  <Provider store={store} >
+    <App />
+  </Provider>;
+
+AppRegistry.registerComponent('tracker', () => AppContainer);
---
+++ tracker/store/reducer.js
@@ -0,0 +1,7 @@
+import * as actionType from './actions';
+
+const state = {
+  exercises: null,
+};
+
+export default const reducer = (state, action)  
