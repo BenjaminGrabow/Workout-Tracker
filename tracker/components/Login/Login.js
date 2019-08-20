206c1e484eb9e2d2cd4c823b44bf01071c684fd1
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-20 20:31:41 +0200

    created 2 new components login and signup

---
+++ tracker/components/Login/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { connect } from 'react-redux';
+import { start, getById } from '../../store/actions';
+
+class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default SignUp;
--- tracker/components/SignUp/SignUp.js
+++ tracker/components/SignUp/SignUp.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { start, getById } from './store/actions';
+import { StyleSheet, Text, View } from 'react-native';
+import { connect } from 'react-redux';
+import { start, getById } from '../../store/actions';
 
-class SignUp extends Component {
+class SignUp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {  }
