33195354cc04a6f0445d103acb453c10b8fc9006
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-20 21:16:15 +0200

    added login component with inputs and login method if button gets pressed

--- tracker/components/Login/Login.js
+++ tracker/components/Login/Login.js
@@ -1,16 +1,54 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
 import { connect } from 'react-redux';
-import { start, getById } from '../../store/actions';
+import { login } from '../../store/actions';
 
-class SignUp extends React.Component {
+class Login extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {
+      username: '',
+      password: '',
+      }
   }
+
+  login = () => {
+this.props.signUp(
+  this.state.username,
+  this.state.password
+);
+
+this.setState({
+  username: '',
+  password: '',
+});
+  };
+ 
   render() { 
-    return (  );
+    return ( 
+      <View style={{padding: 10}}>
+      <TextInput
+         style={{height: 45,width: "95%",borderColor: "gray",borderWidth: 2}}
+        placeholder="Username"
+        onChangeText={(username) => this.setState({username})}
+        value={this.state.username}
+      />
+       <TextInput
+    style={{height: 45,width: "95%",borderColor: "gray",borderWidth: 2}}
+        placeholder="Password"
+        onChangeText={(password) => this.setState({password})}
+        value={this.state.password}
+        underlineColorAndroid="transparent"
+        // Making the Text Input Text Hidden.
+        secureTextEntry={true}
+      />
+      <Button
+            onPress={this.login}
+            title="Sign Up"
+          />
+    </View>
+     );
   }
 }
  
-export default SignUp;
+export default connect(null, { login })(Login);
